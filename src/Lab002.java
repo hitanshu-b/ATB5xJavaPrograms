8b6669eaa107d738a0e98e05283866ea3033faa7
hitanshu-b <hitanshubagde196@gmail.com>
2023-12-24 15:15:20 +0530

    first commit

---
+++ src/Lab002.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        //we cannot use keywords as variables
+        //keywords are always in lower case
+
+        byte age = 10; //range of byte -128 to 127
+        age = 127;
+        System.out.println(age+1);
+        //interview que : it will not give error as "Index out of range"
+
+        short age2 = 129;
+        
+    }
+}
